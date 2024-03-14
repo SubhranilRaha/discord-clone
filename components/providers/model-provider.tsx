f59cd0e9b35fd8cb9c52f73dcffb39e8b05349e3
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2024-03-15 03:03:38 +0530

    manage members

--- components/providers/model-provider.tsx
+++ components/providers/model-provider.tsx
@@ -3,6 +3,8 @@
 import { CreateServerModal } from "@/components/modals/create-server-modal"
 import { useEffect, useState } from "react"
 import { InviteModal } from "../modals/invite-modal"
+import { EditServerModal } from "../modals/edit-server-modal"
+import { MembersModal } from "../modals/members-modal"
 
 export const ModalProvider = () => {
 
@@ -18,6 +20,8 @@ export const ModalProvider = () => {
         <>
         <CreateServerModal />
         <InviteModal/>
+        <EditServerModal/>
+        <MembersModal/>
         </>
     )
 }
